055c863c7b63f63abd1d4c25d06b219019bb73e8
kagencrouch <crouchkagen@gmail.com>
2022-03-14 17:40:40 -0500

    Fixed error display on volunteer opportunity cards

--- lib/user_registration.dart
+++ lib/user_registration.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:volunteer_time_tracking/main.dart';
 import 'package:volunteer_time_tracking/user_account.dart';
@@ -33,49 +35,98 @@ class UserRegistrationPage extends StatefulWidget {
 class _UserRegistrationPage extends State<UserRegistrationPage> {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
-  Widget volunteerCard(
-      String name, String date, String discription, String orginizerName) {
+  Widget volunteerCard(String name, String date, String description,
+      String organizerName, String startTime, String endTime, String loca) {
     return Container(
-      width: displayWidth(context) * .70,
-      padding: const EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        borderRadius: const BorderRadius.all(Radius.circular(5)),
-        color: const Color.fromARGB(255, 167, 206, 238),
-        border: Border.all(color: Colors.grey),
-      ),
-      child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-        Column(children: <Widget>[
-          Container(
-            width: displayWidth(context) * .70,
-            padding: const EdgeInsets.all(10),
-            child: Text(
-              name,
-              textAlign: TextAlign.left,
-              style: const TextStyle(fontSize: 17),
+        width: displayWidth(context) * .70,
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          borderRadius: const BorderRadius.all(Radius.circular(5)),
+          color: const Color.fromARGB(255, 167, 206, 238),
+          border: Border.all(color: Colors.grey),
+        ),
+        child: Column(
+          children: [
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Column(children: [
+                Container(
+                  width: displayWidth(context) * .50,
+                  padding: const EdgeInsets.all(10),
+                  decoration: const BoxDecoration(
+                    color: Color.fromARGB(255, 238, 237, 167),
+                  ),
+                  child: Text(
+                    name,
+                    textAlign: TextAlign.left,
+                    style: const TextStyle(fontSize: 17),
+                  ),
+                ),
+                Container(
+                  width: displayWidth(context) * .50,
+                  padding: const EdgeInsets.all(10),
+                  child: Text(
+                    "Organizer: " + organizerName,
+                    textAlign: TextAlign.left,
+                    style: const TextStyle(fontSize: 13),
+                  ),
+                ),
+              ]),
+              const SizedBox(width: 10),
+              Column(children: [
+                Container(
+                  width: displayWidth(context) * .15,
+                  padding: const EdgeInsets.all(10),
+                  decoration: const BoxDecoration(
+                    color: Color.fromARGB(255, 187, 238, 167),
+                  ),
+                  child: Text(
+                    date,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(fontSize: 13),
+                  ),
+                ),
+                Container(
+                  width: displayWidth(context) * .15,
+                  padding: const EdgeInsets.all(10),
+                  decoration: const BoxDecoration(
+                    color: Color.fromARGB(255, 187, 238, 167),
+                  ),
+                  child: Text(
+                    startTime + "-" + endTime,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(fontSize: 13),
+                  ),
+                ),
+              ]),
+            ]),
+            const SizedBox(height: 10),
+            Container(
+              width: displayWidth(context) * .66,
+              padding: const EdgeInsets.all(10),
+              decoration: const BoxDecoration(
+                color: Color.fromARGB(255, 238, 184, 167),
+              ),
+              child: Text(
+                "Location: " + loca,
+                textAlign: TextAlign.left,
+                style: const TextStyle(fontSize: 13),
+              ),
             ),
-          ),
-          /*Area to display email*/
-          Container(
-            width: displayWidth(context) * .70,
-            padding: const EdgeInsets.all(10),
-            child: Text(
-              date,
-              textAlign: TextAlign.left,
-              style: const TextStyle(fontSize: 15),
-            ),
-          ),
-          Container(
-            width: displayWidth(context) * .70,
-            padding: const EdgeInsets.all(10),
-            child: Text(
-              discription,
-              textAlign: TextAlign.left,
-              style: const TextStyle(fontSize: 15),
-            ),
-          )
-        ])
-      ]),
-    );
+            const SizedBox(height: 10),
+            Container(
+              width: displayWidth(context) * .66,
+              padding: const EdgeInsets.all(10),
+              decoration: const BoxDecoration(
+                color: Color.fromARGB(255, 238, 184, 167),
+              ),
+              child: Text(
+                description,
+                textAlign: TextAlign.left,
+                style: const TextStyle(fontSize: 15),
+              ),
+            )
+          ],
+        ));
   }
 
   Size displaySize(BuildContext context) {
@@ -113,7 +164,14 @@ class _UserRegistrationPage extends State<UserRegistrationPage> {
               ),
             ),
             const SizedBox(height: 10) /*Spacing for user*/,
-            volunteerCard("Opp 1", "01-01-22", "This is a test", "John Doe"),
+            volunteerCard(
+                "Title",
+                "01-01-22",
+                "This is an example description. This is to be used just as a placeholder for a real description for volunteer opportunities. The placeholder also tests the width constrainst of the text box.",
+                "John Doe",
+                "8:00am",
+                "4:00pm",
+                "0000 N Empty St Fayetteville, Arkansas 72701"),
           ],
         ),
       ),

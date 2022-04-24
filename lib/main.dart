7af9d6661771ffc0687ba7752beddb2ba7a3106f
pingyhead <42194751+pingyhead@users.noreply.github.com>
2022-04-24 14:13:50 -0500

    ClockInClockOut revamped

--- lib/main.dart
+++ lib/main.dart
@@ -1,116 +1,252 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+import 'clockinclockout_eventsdropdown.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() => runApp(new MyApp());
+
+// datetime picker documentation: https://pub.dev/packages/flutter_datetime_picker/example
+class CustomPicker extends CommonPickerModel {
+  String digits(int value, int length) {
+    return '$value'.padLeft(length, "0");
+  }
+
+  CustomPicker({DateTime? currentTime, LocaleType? locale})
+      : super(locale: locale) {
+    this.currentTime = currentTime ?? DateTime.now();
+    this.setLeftIndex(this.currentTime.hour);
+    this.setMiddleIndex(this.currentTime.minute);
+    this.setRightIndex(this.currentTime.second);
+  }
+
+  @override
+  String? leftStringAtIndex(int index) {
+    if (index >= 0 && index < 24) {
+      return this.digits(index, 2);
+    } else {
+      return null;
+    }
+  }
+
+  @override
+  String? middleStringAtIndex(int index) {
+    if (index >= 0 && index < 60) {
+      return this.digits(index, 2);
+    } else {
+      return null;
+    }
+  }
+
+  @override
+  String? rightStringAtIndex(int index) {
+    if (index >= 0 && index < 60) {
+      return this.digits(index, 2);
+    } else {
+      return null;
+    }
+  }
+
+  @override
+  String leftDivider() {
+    return "|";
+  }
+
+  @override
+  String rightDivider() {
+    return "|";
+  }
+
+  @override
+  List<int> layoutProportions() {
+    return [1, 2, 1];
+  }
+
+  @override
+  DateTime finalTime() {
+    return currentTime.isUtc
+        ? DateTime.utc(
+            currentTime.year,
+            currentTime.month,
+            currentTime.day,
+            this.currentLeftIndex(),
+            this.currentMiddleIndex(),
+            this.currentRightIndex())
+        : DateTime(
+            currentTime.year,
+            currentTime.month,
+            currentTime.day,
+            this.currentLeftIndex(),
+            this.currentMiddleIndex(),
+            this.currentRightIndex());
+  }
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+// List<DropdownMenuItem<String>> get dropdownItems {
+//   List<DropdownMenuItem<String>> menuItems = [
+//     DropdownMenuItem(child: Text("USA"), value: "USA"),
+//     DropdownMenuItem(child: Text("Canada"), value: "Canada"),
+//     DropdownMenuItem(child: Text("Brazil"), value: "Brazil"),
+//     DropdownMenuItem(child: Text("England"), value: "England"),
+//   ];
+//   return menuItems;
+// }
 
+// class DropdownItem extends StatefulWidget {
+//   const DropdownItem({Key? key}) : super(key: key);
+
+//   @override
+//   _DropdownItemState createState() => _DropdownItemState();
+// }
+
+// class _DropdownItemState extends State<DropdownItem> {
+//   String selectedValue = "USA";
+//   @override
+//   Widget build(BuildContext context) {
+//     return DropdownButton(value: selectedValue, items: dropdownItems);
+//   }
+// }
+
+class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return new MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        //test
+      theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class HomePage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class UserPickedValues {
+  // values are written when user presses "Done" in the clock-in date picker
+  static DateTime datePicked = DateTime.now();
+  static DateTime clockInTime = DateTime.now();
+  static DateTime clockOutTime = DateTime.now();
+}
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+class _HomePageState extends State<HomePage> {
+  void updatePage() {
+    // purpose of this is to get the Event chosen text widget to update the screen with the event picked when the user chooses the event
+    setState(() {});
+  }
+
+  DateTime getCurrentDate() {
+    DateTime now = new DateTime.now();
+    DateTime date = new DateTime(now.year, now.month, now.day);
+    return date;
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Datetime Picker'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            Row(
+              children: [
+                TextButton(
+                    onPressed: () {
+                      Navigator.of(context)
+                          .push(
+                              MaterialPageRoute(builder: (_) => EventPicker()))
+                          .then((value) => updatePage());
+                    },
+                    child: Text('Click to choose event')),
+                Text('Event chosen: ${EventChosen.eventChosen}'),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Row(
+              children: <Widget>[
+                TextButton(
+                    onPressed: () {
+                      DatePicker.showDatePicker(context,
+                          showTitleActions: true,
+                          minTime: DateTime(2022, 1, 1),
+                          maxTime: DateTime(getCurrentDate().year,
+                              getCurrentDate().month, getCurrentDate().day),
+                          // theme: DatePickerTheme(
+                          //     headerColor: Colors.orange,
+                          //     backgroundColor: Colors.blue,
+                          //     itemStyle: TextStyle(
+                          //         color: Colors.white,
+                          //         fontWeight: FontWeight.bold,
+                          //         fontSize: 18),
+                          //     doneStyle:
+                          //         TextStyle(color: Colors.white, fontSize: 16)),
+                          onChanged: (date) {
+                        print('change $date in time zone ' +
+                            date.timeZoneOffset.inHours.toString());
+                      }, onConfirm: (date) {
+                        print('confirm $date');
+                        UserPickedValues.datePicked = date;
+                        updatePage(); // refresh page after user picks value
+                      }, currentTime: DateTime.now(), locale: LocaleType.en);
+                    },
+                    child: Text(
+                      'Pick Date',
+                      style: TextStyle(color: Colors.blue),
+                    )),
+                Text('${UserPickedValues.datePicked}'),
+                Text('      '), // whitespace
+              ],
             ),
+            Row(
+              children: <Widget>[
+                TextButton(
+                    onPressed: () {
+                      DatePicker.showTime12hPicker(context,
+                          showTitleActions: true, onChanged: (date) {
+                        print('change $date in time zone ' +
+                            date.timeZoneOffset.inHours.toString());
+                      }, onConfirm: (date) {
+                        print('confirm $date');
+                        UserPickedValues.clockInTime = date;
+                        updatePage(); // refresh page after user picks value
+                      }, currentTime: DateTime.now());
+                    },
+                    child: Text(
+                      'Pick Clock-In Time',
+                      style: TextStyle(color: Colors.blue),
+                    )),
+                Text('${UserPickedValues.clockInTime}'),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                TextButton(
+                    onPressed: () {
+                      DatePicker.showTime12hPicker(context,
+                          showTitleActions: true, onChanged: (date) {
+                        print('change $date in time zone ' +
+                            date.timeZoneOffset.inHours.toString());
+                      }, onConfirm: (date) {
+                        print('confirm $date');
+                        UserPickedValues.clockOutTime = date;
+                        updatePage(); // refresh the page after user picks value
+                      }, currentTime: DateTime.now());
+                    },
+                    child: Text(
+                      'Pick Clock-Out Time',
+                      style: TextStyle(color: Colors.blue),
+                    )),
+                Text('${UserPickedValues.clockOutTime}'),
+              ],
+            ),
+            TextButton(onPressed: () {}, child: Text('Submit'))
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
